6c73a338dd4eb287b157f66890928a99e6da9224
GenRevo89 <krishnapatel@mac.com>
2026-02-25 19:30:32 -0700

    feat: enable and configure `postcss-preset-env` with updated stage and feature flags.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,23 +1,24 @@
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
-    /*
     "postcss-preset-env": {
-      stage: 1, // Transpile modern drafting stage CSS
+      stage: 3,
       features: {
-        "nesting-rules": false, // Handled implicitly by Tailwind 
-        "cascade-layers": true, // Critical for Chrome < 99 (VP550)
-        "color-mix": true, // Critical for Chrome < 111
+        "nesting-rules": false,
+        "cascade-layers": false,
+        "media-query-ranges": false,
+        "custom-properties": false,
+        "color-mix": true,
         "oklab-function": true,
+        "color-function": true
       },
       browsers: [
-        "Chrome >= 70", // Target older Android Systems WebViews (NDroid)
+        "Chrome >= 70",
         "Safari >= 12",
         "last 2 versions",
         "not dead"
       ]
     }
-    */
   }
 };
 
