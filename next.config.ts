3a8ab15355161e7572f975411a0947226a79b4d8
GenRevo89 <krishnapatel@mac.com>
2026-05-28 17:25:08 -0600

    v1.7.6zzn - COOP header policy fixed.

--- next.config.ts
+++ next.config.ts
@@ -114,7 +114,7 @@ const nextConfig = {
           },
           {
             key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
+            value: "unsafe-none",
           },
         ],
       },
